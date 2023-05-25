db5d94ad27b846ea2bd09df703fb7cebff16e29c
oh-yeongji <dev.cappp@gmail.com>
2023-05-25 17:23:46 +0900

    feat:헤더 반응형및 1차 완료

--- js/header.js
+++ js/header.js
@@ -1,31 +1,62 @@
-// 웹브라우저에 html,css,js, image를
-// 모두 불러들여서 렌더링 준비가 끝나면
-// 그때 function의 블럭 안쪽 {}를 실행!
-window.onload = function () {
-  // header nav에 마우스 오버하면 header높이 260px변화주기
-  // nav에 마우스 아웃하면 header 높이 100px변경하기
-  // header를 js로 저장해보자(변수 정의해보자)
-  let header = document.querySelector(".header");
-  // nav를 js 로 저장해보자 (변수 정의해보자)
-  let nav = document.querySelector(".nav");
-  // console.log(header);
-  // console.log(nav);
+window.addEventListener("load", function () {
+  // 모바일 버튼 기능
 
-  // nav에 마우스올린다.(마우스오버처리)
-  // html요소에 마우스 오버 처리하는법
+  // nav 에 마우스 오버하면 header 높이 260px 변경주기
+  // nav 에 마우스 아웃하면 header 높이 100px 변경하기
+  // header 를 js 로 저장해 보자(변수 정의해 보자)
+  let header = document.querySelector(".header");
+  // nav 를 js 로 저장해 보자(변수 정의해 보자)
+  let nav = document.querySelector(".nav");
+  //   console.log(header);
+  //   console.log(nav);
+
+  //   nav 에 마우스 오버 처리
+  //  html 요소에 마우스 오버 처리하는 법
   nav.addEventListener("mouseenter", function () {
+    // header 의 높이를 260 픽셀로 고치고 싶다.
     // header.style.height = "260px";
-    // 우리는260픽셀로 변경되는 class를 만들어두었다.
+    // 우리는 260 픽셀로 변경되는 class 를 작성해 두었다.
     header.classList.add("header-active");
   });
-  // haeder 의 높이를 260픽셀로 고치고 싶다.
-
-  // nav에 마우스 아웃 처리
-  // html요소에 마우스 아웃처리하는법
+  //   nav 에 마우스 아웃 처리
+  //  html 요소에 마우스 아웃 처리하는 법
   nav.addEventListener("mouseleave", function () {
-    // header의 높이를 100px로 고치고싶다.
+    // header 의 높이를 100 픽셀로 고치고 싶다.
     // header.style.height = "100px";
     // 클래스를 제거한다.
     header.classList.remove("header-active");
   });
-};
+
+  let gnbA = document.querySelectorAll(".gnb > li");
+  let navBlueBar = document.querySelector(".nav-blue-bar");
+
+  // 최초 위치 및 너비
+  let posX = gnbA[0].getBoundingClientRect().left;
+  let widthX = gnbA[0].getBoundingClientRect().width;
+  navBlueBar.style.left = posX + "px";
+  navBlueBar.style.width = widthX + "px";
+
+  gnbA.forEach((item) => {
+    item.addEventListener("mouseenter", function (event) {
+      let posX = this.getBoundingClientRect().left; //this =item
+      let widthX = this.getBoundingClientRect().width;
+      // navBlueBar.style.left = posX + "px";
+      // navBlueBar.style.width = widthX + "px";
+      anime({
+        targets: navBlueBar,
+        left: posX,
+        width: widthX,
+        easing: "easeInOutQuad",
+        duration: 500,
+      });
+    });
+
+    // console.log(item.getBoundingClientRect());
+    // console.log(posx);
+  });
+  // a.forEach(function (item) {
+  //   console.log(item);
+  //   console.log(item.getBoundingClientRect().left);
+  //   console.log(item.offsetWidth);
+  // });
+});
