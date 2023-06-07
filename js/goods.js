05d33cb2cd2bba93c28dfcb7c73dbb7c0a1b30dd
dev.cappp <dev.cappp@gmail.com>
2023-06-07 18:23:20 +0900

    feat: company section 작업 중

--- js/goods.js
+++ js/goods.js
@@ -23,8 +23,8 @@ window.addEventListener("load", function () {
     let html = ``;
     let copyArr = [..._data.goods];
 
-    //swiper 버전에 따른 문제 발생
-    //강제 목록 추가 제거
+    // Swiper 버전에 따른 문제 발생
+    // 강제 목록 추가 제거
     // if (copyArr.length <= SLIDECOUNT) {
     //   copyArr = [..._data.goods, ..._data.goods];
     // }
@@ -34,8 +34,8 @@ window.addEventListener("load", function () {
         <div class="swiper-slide">
           <a href="${item.link}" class="good-link">
             <div class="good-item">
-              <div class="good-item-img">
-                <img src="images/${item.image}" alt="${item.alt}" />
+              <div class="good-item-img" style="background-image:url(images/${item.image});">
+                
               </div>
               <div class="good-item-txt">
                 <p>${item.title}</p>
@@ -56,14 +56,14 @@ window.addEventListener("load", function () {
       loop: true,
       speed: 1000,
       slidesPerView: 3,
-      spaceBetween: 20,
-      navigation: {
-        prevEl: ".sw-goods-prev",
-        nextEl: ".sw-goods-next",
-      },
+      spaceBetween: 50,
       autoplay: {
-        delay: 1000,
+        delay: 2500,
         disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".sw-goods-next",
+        prevEl: ".sw-goods-prev",
       },
       breakpoints: {
         480: {
@@ -71,21 +71,24 @@ window.addEventListener("load", function () {
           spaceBetween: 30,
         },
         768: {
-          slidesPerView: 3,
+          slidesPerView: 2,
           spaceBetween: 30,
         },
         1400: {
           slidesPerView: 3,
-          spaceBetween: 50,
+          spaceBetween: 30,
+        },
+        1600: {
+          slidesPerView: 3,
+          spaceBetween: 30,
         },
       },
     });
     swGoods.on("slideChange", function () {
       // let count = this.realIndex % SLIDECOUNT;
+      // focusMenu(count);
       focusMenu(this.realIndex);
     });
-    console.log("gogo");
-    focusMenu(0);
   }
 
   function focusMenu(_index) {
@@ -119,14 +122,15 @@ window.addEventListener("load", function () {
         swGoods.slideToLoop(index);
       };
     });
+
     focusMenu(0);
   }
 
   getData();
 
   // 슬라이드 멈추기/재생하기
-  let bt = document.querySelector(".sw-goods-pause");
-  let icon = bt.querySelector(".fa-pause");
+  const bt = document.querySelector(".sw-goods-pause");
+  const icon = bt.querySelector(".fa-pause");
 
   let swGoodsState = "play";
   bt.onclick = (event) => {
